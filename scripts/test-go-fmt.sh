4d7da6b58d0f63f548dfe0311653ad383269da3c
Adam Ross <aross@phase2technology.com>
2017-05-17 15:43:38 -0700

    Add initial travis CI implementation for go fmt.

---
+++ scripts/test-go-fmt.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+##
+# Test go fmt
+#
+# Test if the codebase has valid go formatting.
+#
+# @see https://github.com/limetext/lime/pull/265/files
+##
+
+fmt="$(find . ! \( -path './3rdparty' -prune \) -type f -name '*.go' -print0 | xargs -0 gofmt -l )"
+
+if [ -n "$fmt" ]; then
+    echo "Unformatted Go source code:"
+    echo "$fmt"
+    exit 1
+fi
