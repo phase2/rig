d58306cb1b03ca6432336c179a038b8960a93907
Chris Johnson <git@tekante.com>
2017-05-19 12:11:21 -0400

    DEVTOOLS-229: Adding project create command

---
+++ cli/commands/project_create.go
@@ -0,0 +1,71 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/phase2/rig/cli/util"
+	"github.com/urfave/cli"
+)
+
+type ProjectCreate struct {
+	BaseCommand
+}
+
+func (cmd *ProjectCreate) Commands() []cli.Command {
+	create := cli.Command{
+		Name:        "create",
+		Aliases:     []string{},
+		Usage:       "Run a yeoman generator in a container to create project configuration files",
+		ArgsUsage:   "[optional type] [optional args]",
+		Description: "The type is the generator to run with args passed to that generator. If using flag arguments use -- before specifying type and arguments.",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "image",
+				Usage: "Docker image to use if default outrigger/generator is not desired",
+			},
+		},
+		Before:      cmd.Before,
+		Action:      cmd.Create,
+	}
+
+	return []cli.Command{create}
+}
+
+// Run a docker image to execute the desired generator
+func (cmd *ProjectCreate) Create(ctx *cli.Context) error {
+	image := ctx.String("image")
+	if (image == "") {
+		image = "outrigger/generator"
+	}
+
+	cwd, err := os.Getwd();
+	if (err != nil) {
+		cmd.out.Error.Printf("Couldn't determine current working directory: %s", err)
+		os.Exit(1)
+	}
+
+	cmd.out.Verbose.Printf("Executing container %s with arguments %s", image, strings.Join(ctx.Args(), " "))
+
+	// keep passed in args as distinct elements or they will be treated as
+	// a single argument containing spaces when the container gets them
+	args := []string{
+		"run",
+		"--rm",
+		"-it",
+		"-v", fmt.Sprintf("%s:/generated", cwd),
+		image,
+		"yo", "--no-insight",
+	}
+	args = append(args, ctx.Args()...)
+
+	shellCmd := exec.Command("docker", args...)
+	if exitCode := util.PassthruCommand(shellCmd); exitCode != 0 {
+		cmd.out.Error.Printf("Error running generator '%s %s': %d", image, strings.Join(ctx.Args(), " "), exitCode)
+		os.Exit(exitCode)
+	}
+
+	return nil
+}
