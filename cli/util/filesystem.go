78d1c7ed4fd37edcc57d6f61e1beb6303b3098de
Adam Ross <aross@phase2technology.com>
2017-10-23 12:25:42 -0700

    go fmt cleanups.

--- cli/util/filesystem.go
+++ cli/util/filesystem.go
@@ -1,9 +1,9 @@
 package util
 
 import (
-  "fmt"
-  "os"
-  "path/filepath"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/kardianos/osext"
 )
@@ -15,20 +15,20 @@ func GetExecutableDir() (string, error) {
 
 // AbsJoin joins the two path segments, ensuring they form an absolute path.
 func AbsJoin(baseDir string, suffixPath string) (string, error) {
-  if absoluteBaseDir, err := filepath.Abs(baseDir); err != nil {
-    return "", fmt.Errorf("Unrecognized working directory: %s: %s", baseDir, err.Error())
-  } else {
-    return filepath.Join(absoluteBaseDir, suffixPath), nil
-  }
+	if absoluteBaseDir, err := filepath.Abs(baseDir); err != nil {
+		return "", fmt.Errorf("Unrecognized working directory: %s: %s", baseDir, err.Error())
+	} else {
+		return filepath.Join(absoluteBaseDir, suffixPath), nil
+	}
 }
 
 // RemoveFile removes the designated file relative to the Working Directory.
 func RemoveFile(pathToFile string, workingDir string) error {
-  if absoluteFilePath, err := AbsJoin(workingDir, pathToFile); err != nil {
-  	return err
-  } else {
-    return os.Remove(absoluteFilePath)
-  }
+	if absoluteFilePath, err := AbsJoin(workingDir, pathToFile); err != nil {
+		return err
+	} else {
+		return os.Remove(absoluteFilePath)
+	}
 
 	return nil
 }
@@ -36,18 +36,18 @@ func RemoveFile(pathToFile string, workingDir string) error {
 // TouchFile creates an empty file, usually for temporary use.
 // @see https://stackoverflow.com/questions/35558787/create-an-empty-text-file/35558965
 func TouchFile(pathToFile string, workingDir string) error {
-  if absoluteFilePath, err := AbsJoin(workingDir, pathToFile); err != nil {
-    return err
-  } else {
-    // If the file already exists there will be no error.
-    if f, err := os.OpenFile(absoluteFilePath, os.O_RDONLY|os.O_CREATE, 0666); err != nil {
-      return fmt.Errorf("Could not touch file: %s: %s", absoluteFilePath, err.Error())
-    } else {
-      // Not checking for an error here because we are not very currently concerned
-      // with file descriptor leaks
-      f.Close()
-    }
-  }
+	if absoluteFilePath, err := AbsJoin(workingDir, pathToFile); err != nil {
+		return err
+	} else {
+		// If the file already exists there will be no error.
+		if f, err := os.OpenFile(absoluteFilePath, os.O_RDONLY|os.O_CREATE, 0666); err != nil {
+			return fmt.Errorf("Could not touch file: %s: %s", absoluteFilePath, err.Error())
+		} else {
+			// Not checking for an error here because we are not very currently concerned
+			// with file descriptor leaks
+			f.Close()
+		}
+	}
 
-  return nil
+	return nil
 }
