3a51e301ed587ba64b239560eea964dd94e407b7
Adam Ross <aross@phase2technology.com>
2017-10-25 15:13:02 -0700

    Fix code formatting.

--- cli/testing/testing.go
+++ cli/testing/testing.go
@@ -3,15 +3,16 @@
 package testing
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
-    "strings"
-    "testing"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
 )
 
 // ExecMockSet provides a set of unique mocks. The key matches the remote execution script.
 type ExecMockSet map[string]string
+
 // ExecMockCollection is a map of ExecMockSets. The key is a category of mocks, such as
 // "success" or "notfound".
 type ExecMockCollection map[string]ExecMockSet
@@ -21,12 +22,11 @@ var mock ExecMockCollection
 // SetMockValues provides an easy setter that allows the TestMain implementation
 // of individual test files to preload the potential values to use.
 func SetMockByType(namespace string, mockSet ExecMockSet) {
-  if mock == nil {
-    mock = make(ExecMockCollection)
-  }
-  mock[namespace] = mockSet
+	if mock == nil {
+		mock = make(ExecMockCollection)
+	}
+	mock[namespace] = mockSet
 }
-
 
 // TestMain is a special function that takes over handling the behavior of the the test runner `go test`
 // generates to execute your code. I do not know if you can have one per file or one for a project's entire
@@ -46,53 +46,53 @@ func SetMockByType(namespace string, mockSet ExecMockSet) {
 //
 // testing.MainTestProcess(m)
 func MainTestProcess(m *testing.M) {
-    switch os.Getenv("GO_TEST_MODE") {
-    case "":
-        // Normal test mode.
-        os.Exit(m.Run())
+	switch os.Getenv("GO_TEST_MODE") {
+	case "":
+		// Normal test mode.
+		os.Exit(m.Run())
 
-    case "echo":
-      	// Outputs the arguments passed to the test runner.
-      	// This will be the command that would have executed under normal runtime.
-      	// This mode can be used to test that we can predict programmatically assembled command that would be executed.
-        fmt.Println(strings.Join(os.Args[1:], " "))
+	case "echo":
+		// Outputs the arguments passed to the test runner.
+		// This will be the command that would have executed under normal runtime.
+		// This mode can be used to test that we can predict programmatically assembled command that would be executed.
+		fmt.Println(strings.Join(os.Args[1:], " "))
 
-    case "mock":
-        if mock != nil {
-            // Used the command that would be executed under normal runtime as the key to our mock value map and outputs the value.
-            // I am still researching how to adjust this overall pattern to centralize the code as test helpers but allow individual
-    	      // test files to supply their own mock.
-            fmt.Printf("%s", mock["success"][strings.Join(os.Args[1:], " ")])
-        }
-    }
+	case "mock":
+		if mock != nil {
+			// Used the command that would be executed under normal runtime as the key to our mock value map and outputs the value.
+			// I am still researching how to adjust this overall pattern to centralize the code as test helpers but allow individual
+			// test files to supply their own mock.
+			fmt.Printf("%s", mock["success"][strings.Join(os.Args[1:], " ")])
+		}
+	}
 }
 
 // mockExecCommand uses fakeExecCommand to transform the intended remote executation
 // into something controlled by the test runner, then adds an environment variable to
 // the command so TestMain routes it to the mocking functionality.
-func MockExecCommand(command string, args...string) *exec.Cmd {
-  cmd := fakeExecCommand(command, args...)
-  cmd.Env = append(cmd.Env, "GO_TEST_MODE=mock")
-  return cmd
+func MockExecCommand(command string, args ...string) *exec.Cmd {
+	cmd := fakeExecCommand(command, args...)
+	cmd.Env = append(cmd.Env, "GO_TEST_MODE=mock")
+	return cmd
 }
 
 // echoExecCommand uses fakeExecCommand to transform the intended remote executation
 // into something controlled by the test runner, then adds an environment variable to
 // the command so TestMain routes it to the command echo functionality.
-func EchoExecCommand(command string, args...string) *exec.Cmd {
-  cmd := fakeExecCommand(command, args...)
-  cmd.Env = append(cmd.Env, "GO_TEST_MODE=echo")
-  return cmd
+func EchoExecCommand(command string, args ...string) *exec.Cmd {
+	cmd := fakeExecCommand(command, args...)
+	cmd.Env = append(cmd.Env, "GO_TEST_MODE=echo")
+	return cmd
 }
 
 // fakeExecCommand creates a new reference to an exec.Cmd object which has been transformed
 // to use the supplied parameters as arguments to be submitted to our test runner binary.
 // It should never be used directly.
-func fakeExecCommand(command string, args...string) *exec.Cmd {
-  testArgs := []string{command}
-  testArgs = append(testArgs, args...)
-  cmd := exec.Command(os.Args[0], testArgs...)
-  cmd.Env = []string{}
+func fakeExecCommand(command string, args ...string) *exec.Cmd {
+	testArgs := []string{command}
+	testArgs = append(testArgs, args...)
+	cmd := exec.Command(os.Args[0], testArgs...)
+	cmd.Env = []string{}
 
-  return cmd
+	return cmd
 }
--- cli/util/logger_test.go
+++ cli/util/logger_test.go
@@ -1,19 +1,19 @@
 package util
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestLoggerInit(t *testing.T) {
-  LoggerInit(false)
-  logger := Logger()
-  if logger.IsVerbose {
-    t.Error("Logger initialized in non-Verbose mode.")
-  }
+	LoggerInit(false)
+	logger := Logger()
+	if logger.IsVerbose {
+		t.Error("Logger initialized in non-Verbose mode.")
+	}
 
-  LoggerInit(true)
-  logger = Logger()
-  if !logger.IsVerbose {
-    t.Error("Logger initialized in Verbose mode.")
-  }
+	LoggerInit(true)
+	logger = Logger()
+	if !logger.IsVerbose {
+		t.Error("Logger initialized in Verbose mode.")
+	}
 }
