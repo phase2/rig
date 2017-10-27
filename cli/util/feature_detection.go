dc0840dac1152a04573ffa35f32dedf9e84ec72b
Frank Febbraro <frankfebbraro@gmail.com>
2017-10-26 20:18:30 -0700

    go fmt

--- cli/util/feature_detection.go
+++ cli/util/feature_detection.go
@@ -1,11 +1,10 @@
 package util
 
 import (
-  "runtime"
+	"runtime"
 )
 
 // Does the runtime OS support docker natively, versus needing to run docker in a virtual machine
 func SupportsNativeDocker() bool {
-  return runtime.GOOS == "linux"
+	return runtime.GOOS == "linux"
 }
-
