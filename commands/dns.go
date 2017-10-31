c8d77ae92510d36f8049f664214a2338238aabb5
Frank Febbraro <frank@phase2technology.com>
2017-10-31 15:30:29 -0700

    added linux support for NetworkManager/dnsmasq and libnss-resolver to the DNS command

--- commands/dns.go
+++ commands/dns.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
-	"runtime"
 	"strings"
 
 	"github.com/phase2/rig/util"
@@ -42,10 +41,11 @@ func (cmd *DNS) Run(c *cli.Context) error {
 	cmd.out.Info.Println("Configuring DNS")
 
 	if util.SupportsNativeDocker() {
-		cmd.ConfigureDNS(cmd.machine, c.String("nameservers"))
+		cmd.StartDNS(cmd.machine, c.String("nameservers"))
+		return cmd.ConfigureLinux()
 	} else if cmd.machine.IsRunning() {
-		cmd.ConfigureDNS(cmd.machine, c.String("nameservers"))
-		cmd.ConfigureRoutes(cmd.machine)
+		cmd.StartDNS(cmd.machine, c.String("nameservers"))
+		cmd.ConfigureDNS(cmd.machine)
 	} else {
 		return cmd.Error(fmt.Sprintf("Machine '%s' is not running.", cmd.machine.Name), "MACHINE-STOPPED", 12)
 	}
@@ -77,40 +77,78 @@ func (cmd *DNS) RemoveHostFilter(ipAddr string) {
 	util.StreamCommand(exec.Command("sudo", "ifconfig", iface, "-hostfilter", member))
 }
 
-// ConfigureRoutes will configure routing to allow access to containers on IP addresses
+// ConfigureDNS will configure routing to allow access to containers on IP addresses
 // within the Docker Machine bridge network
-func (cmd *DNS) ConfigureRoutes(machine Machine) {
+func (cmd *DNS) ConfigureDNS(machine Machine) {
 	cmd.out.Info.Println("Setting up local networking (may require your admin password)")
 
-	machineIP := machine.GetIP()
-	bridgeIP := machine.GetBridgeIP()
-	if runtime.GOOS == "windows" {
-		exec.Command("runas", "/noprofile", "/user:Administrator", "route", "DELETE", "172.17.0.0").Run()
-		util.StreamCommand(exec.Command("runas", "/noprofile", "/user:Administrator", "route", "-p", "ADD", "172.17.0.0/16", machineIP))
+	if util.IsWindows() {
+		cmd.ConfigureWindows(machine)
 	} else if util.IsMac() {
-		if machine.IsXhyve() {
-			cmd.RemoveHostFilter(machine.GetIP())
-		}
-		exec.Command("sudo", "mkdir", "-p", "/etc/resolver").Run()
-		exec.Command("bash", "-c", "echo \"nameserver "+bridgeIP+"\" | sudo tee /etc/resolver/vm").Run()
-		exec.Command("sudo", "route", "-n", "delete", "-net", "172.17.0.0").Run()
-		util.StreamCommand(exec.Command("sudo", "route", "-n", "add", "172.17.0.0/16", machineIP))
-
-		if _, err := os.Stat("/usr/sbin/discoveryutil"); err == nil {
-			// Put this here for people running OS X 10.10.0 to 10.10.3 (oy vey.)
-			cmd.out.Verbose.Println("Restarting discoveryutil to flush DNS caches")
-			util.StreamCommand(exec.Command("sudo", "launchctl", "unload", "-w", "/System/Library/LaunchDaemons/com.apple.discoveryd.plist"))
-			util.StreamCommand(exec.Command("sudo", "launchctl", "load", "-w", "/System/Library/LaunchDaemons/com.apple.discoveryd.plist"))
-		} else {
-			// Reset DNS cache. We have seen this suddenly make /etc/resolver/vm work.
-			cmd.out.Verbose.Println("Restarting mDNSResponder to flush DNS caches")
-			util.StreamCommand(exec.Command("sudo", "killall", "-HUP", "mDNSResponder"))
-		}
+		cmd.ConfigureMac(machine)
 	}
 }
 
-// ConfigureDNS will start the dnsdock service
-func (cmd *DNS) ConfigureDNS(machine Machine, nameservers string) {
+// ConfigureWindows configures network routing
+func (cmd *DNS) ConfigureWindows(machine Machine) {
+	exec.Command("runas", "/noprofile", "/user:Administrator", "route", "DELETE", "172.17.0.0").Run()
+	util.StreamCommand(exec.Command("runas", "/noprofile", "/user:Administrator", "route", "-p", "ADD", "172.17.0.0/16", machine.GetIP()))
+}
+
+// ConfigureMac configures DNS resolution and network routing
+func (cmd *DNS) ConfigureMac(machine Machine) {
+	machineIP := machine.GetIP()
+	bridgeIP := machine.GetBridgeIP()
+
+	if machine.IsXhyve() {
+		cmd.RemoveHostFilter(machine.GetIP())
+	}
+	exec.Command("sudo", "mkdir", "-p", "/etc/resolver").Run()
+	exec.Command("bash", "-c", "echo \"nameserver "+bridgeIP+"\" | sudo tee /etc/resolver/vm").Run()
+	exec.Command("sudo", "route", "-n", "delete", "-net", "172.17.0.0").Run()
+	util.StreamCommand(exec.Command("sudo", "route", "-n", "add", "172.17.0.0/16", machineIP))
+	if _, err := os.Stat("/usr/sbin/discoveryutil"); err == nil {
+		// Put this here for people running OS X 10.10.0 to 10.10.3 (oy vey.)
+		cmd.out.Verbose.Println("Restarting discoveryutil to flush DNS caches")
+		util.StreamCommand(exec.Command("sudo", "launchctl", "unload", "-w", "/System/Library/LaunchDaemons/com.apple.discoveryd.plist"))
+		util.StreamCommand(exec.Command("sudo", "launchctl", "load", "-w", "/System/Library/LaunchDaemons/com.apple.discoveryd.plist"))
+	} else {
+		// Reset DNS cache. We have seen this suddenly make /etc/resolver/vm work.
+		cmd.out.Verbose.Println("Restarting mDNSResponder to flush DNS caches")
+		util.StreamCommand(exec.Command("sudo", "killall", "-HUP", "mDNSResponder"))
+	}
+}
+
+// ConfigureLinux configures network routing and DNS resolution
+func (cmd *DNS) ConfigureLinux() error {
+	bridgeIP, err := util.GetBridgeIP()
+	if err != nil {
+		return err
+	}
+
+	// Is NetworkManager in use
+	if _, err := os.Stat("/etc/NetworkManager/dnsmasq.d"); err == nil {
+		// Install for NetworkManager/dnsmasq connection to dnsdock
+		util.StreamCommand(exec.Command("bash", "-c", fmt.Sprintf("echo 'server=/vm/%s' | sudo tee /etc/NetworkManager/dnsmasq.d/dnsdock.conf", bridgeIP)))
+
+		// Restart NetworkManager if it is running
+		if err := exec.Command("systemctl", "is-active", "NetworkManager").Run(); err != nil {
+			util.StreamCommand(exec.Command("sudo", "systemctl", "restart", "NetworkManager"))
+		}
+	}
+
+	// Is libnss-resolver in use
+	if _, err := os.Stat("/etc/resolver"); err == nil {
+		// Install for libnss-resolver connection to dnsdock
+		exec.Command("bash", "-c", fmt.Sprintf("echo 'nameserver %s' | sudo tee /etc/resolver/vm", bridgeIP)).Run()
+	}
+
+	return nil
+}
+
+
+// StartDNS will start the dnsdock service
+func (cmd *DNS) StartDNS(machine Machine, nameservers string) {
 	dnsServers := strings.Split(nameservers, ",")
 
 	// Linux uses standard bridge IP
@@ -132,7 +170,7 @@ func (cmd *DNS) ConfigureDNS(machine Machine, nameservers string) {
 		"-l", "com.dnsdock.name=dnsdock",
 		"-l", "com.dnsdock.image=outrigger",
 		"--name", "dnsdock",
-		"-p", bridgeIP + ":53:53/udp",
+		"-p", fmt.Sprintf("%s:53:53/udp", bridgeIP),
 		"aacebedo/dnsdock:v1.16.1-amd64",
 		"--domain=vm",
 	}
--- commands/start.go
+++ commands/start.go
@@ -93,7 +93,7 @@ func (cmd *Start) Run(c *cli.Context) error {
 
 	cmd.out.Info.Println("Setting up DNS...")
 	dns := DNS{BaseCommand{machine: cmd.machine, out: cmd.out}}
-	dns.ConfigureDNS(cmd.machine, c.String("nameservers"))
+	dns.StartDNS(cmd.machine, c.String("nameservers"))
 
 	// NFS mounts are Mac-only.
 	if util.IsMac() {
@@ -128,7 +128,7 @@ func (cmd *Start) Run(c *cli.Context) error {
 		return cmd.Error(err.Error(), "DATA-MOUNT-FAILED", 13)
 	}
 
-	dns.ConfigureRoutes(cmd.machine)
+	dns.ConfigureDNS(cmd.machine)
 
 	cmd.out.Verbose.Println("Use docker-machine to interact with your virtual machine.")
 	cmd.out.Verbose.Printf("For example, to SSH into it: docker-machine ssh %s", cmd.machine.Name)
@@ -145,7 +145,7 @@ func (cmd *Start) Run(c *cli.Context) error {
 // a virtual machine and networking is not required or managed by Outrigger.
 func (cmd *Start) StartMinimal(nameservers string) error {
 	dns := DNS{BaseCommand{machine: cmd.machine, out: cmd.out}}
-	dns.ConfigureDNS(cmd.machine, nameservers)
+	dns.StartDNS(cmd.machine, nameservers)
 
 	dash := Dashboard{BaseCommand{machine: cmd.machine, out: cmd.out}}
 	dash.LaunchDashboard(cmd.machine)
